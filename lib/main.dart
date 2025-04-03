d8904f582962936ce0a5a619aaa68ae426405281
ThiagoNogueira2 <thiagonogueira_2003@hotmail.com>
2025-04-03 17:06:20 -0300

    Tentando conexão com o Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'registrer.dart'; // Adicionando a importação do register.dart
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://sxkswxpzhkjqbfccmeam.supabase.co', //Mesma chave sendo chamada
+    // Chave pegada do meu banco de dados, está sendo interligada de acordo com isso
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4a3N3eHB6aGtqcWJmY2NtZWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2ODEyNTYsImV4cCI6MjA1OTI1NzI1Nn0.yWXoehqNvf9fWXPiBTh0Q7ABieIbm1OF2J1TrscnFLU', // Chave anônima do seu projeto
+  );
+
+  runApp(MyApp()); //Aqui nos temos meu aplicativo sendo chamado
+  //De acordo com o registrer irá ter a construção do app com o forms
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      title: 'Flutter App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: RegisterScreen(), //
     );
   }
 }
